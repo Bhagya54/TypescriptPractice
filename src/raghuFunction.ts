82e2507c5c5b4fe698b61c28797f81592bddfe4d
Raghupathi Reddy <sppathi001@gmail.com>
2026-05-01 14:03:07 +0530

    update new function

---
+++ src/raghuFunction.ts
@@ -0,0 +1,16 @@
+//function productCost (price:number, discount:number) {
+ //   console.log(price-discount);
+//}
+//productCost(400, 30);
+
+//function to chek if a number is even
+
+function evenNo(nub1:number){
+    if (nub1%2==0){
+        console.log(`given number is evn num`);
+    }
+    else {
+        console.log(`given num is add`)
+    }
+}
+evenNo(20);
